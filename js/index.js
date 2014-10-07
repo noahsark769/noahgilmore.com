05c08377f8a6897b66c18291896c7a70e7dcb67e
Noah Gilmore <noah.w.gilmore@gmail.com>
2014-10-07 15:37:12 -0700

    transitions are working, about looks crappy, but meh

--- js/index.js
+++ js/index.js
@@ -1,8 +1,21 @@
-var transitionTo = function () {};
+var transitionTo = function (sectionKey) {
+    console.log("transitionTo called with key: ", sectionKey);
+    var activeSection = $(".js-is-active-section");
+    console.log(activeSection.get(0));
+    activeSection.fadeOut(function () {
+        activeSection.removeClass("js-is-active-section");
+        activeSection.removeClass("current");
+        var nextSection = $(".js-section-" + sectionKey);
+        console.log(nextSection.get(0));
+        nextSection.addClass("js-is-active-section");
+        nextSection.addClass("current");
+        nextSection.fadeIn();
+    });
+};
 
 $(document).ready(function () {
     $(".link a").click(function () {
-        var pageKey = $(this).data("page");
-        transitionTo("pageKey");
+        var sectionKey = $(this).data("section");
+        transitionTo(sectionKey);
     });
 });
