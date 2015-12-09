0d81bd56c6d11423ee76d8e50b5d00bc903ebfb6
Noah Gilmore <noah.w.gilmore@gmail.com>
2015-12-08 23:19:53 -0800

    having way too much fun writing a push script

--- scripts/push.py
+++ scripts/push.py
@@ -1,5 +1,27 @@
 import subprocess
 
+class Colors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+def print_info(string):
+  return print_color(Colors.OKBLUE, string)
+
+def print_success(string):
+  return print_color(Colors.OKGREEN, string)
+
+def print_fail(string):
+  return print_color(Colors.FAIL, string)
+
+def print_color(color, string):
+  print color + string + Colors.ENDC
+
 def check_return_code(command_list):
   return subprocess.Popen(command_list).wait()
 
@@ -9,11 +31,11 @@ def exec_string(command_string):
 def exec_strings(commands):
   """Execute a series of commands and stop if one of them has non-zero return code."""
   for command in commands:
-    print "----> [Exec] %s" % command
+    print_info("[Exec] %s" % command)
     if exec_string(command) != 0:
-      print "[Error] Command `%s` did not return exit status 0." % command
+      print_fail("[Error] Command `%s` did not return exit status 0." % command)
       return
-  print "[Info] All commands completed successfully."
+  print_success("[Info] All commands completed successfully.")
 
 if __name__ == "__main__":
   exec_strings([
