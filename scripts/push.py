b6ec4d6dd463e1bd5b9620f3a50dd27309c259c5
Noah Gilmore <noah.w.gilmore@gmail.com>
2015-12-08 23:08:09 -0800

    Fix a bug in the push script

--- scripts/push.py
+++ scripts/push.py
@@ -4,7 +4,7 @@ def check_return_code(command_list):
   return subprocess.Popen(command_list).wait()
 
 def exec_string(command_string):
-  return check_return_code(command.strip().split())
+  return check_return_code(command_string.strip().split())
 
 def exec_strings(commands):
   """Execute a series of commands and stop if one of them has non-zero return code."""
