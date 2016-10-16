fce39a0fb05272594eebc1cd3da7734426d6d95a
Noah Gilmore <noah@yelp.com>
2016-10-16 16:18:35 -0700

    delete all the react stuff

--- blog/jsx/post.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import PostApplication from './PostApplication';
-
-ReactDOM.render(<PostApplication />, document.getElementById('app'));
