57150b10ab8e26d47d7644a35fe5d8062b6a4fb4
Noah Gilmore <noah@yelp.com>
2017-02-18 16:12:14 -0800

    Some stuff and untitled.ipynb

--- scripts/push.py
+++ scripts/push.py
@@ -1,3 +1,5 @@
+"""Script to push changes to gh-pages."""
+
 from simplegit import Git
 from logger import Logger
 
@@ -9,22 +11,23 @@ git = Git(logger=logger)
 
 
 def get_current_branch():
+    """Return the current branch name."""
     lines, _, _ = git.call("rev-parse", "--abbrev-ref HEAD")
     return lines[0].strip()
 
 
 def has_uncommitted_changes():
-    """Return whether the current branch has uncommitted changes. Note that this is not
-    technically the best possible way to do this, but it works for our purposes.
+    """Return whether the current branch has uncommitted changes.
+
+    Note that this is not technically the best possible way to do this, but it
+    works for our purposes.
     """
     lines, _, _ = git.status("-s")
     return bool(len(lines))
 
 
 def exec_steps(*args):
-    """Given step functions as args, executes them in order until one returns
-    False.
-    """
+    """Given step functions as args, execute in order until False returned."""
     for step in args:
         if not step():
             logger.fatal("Step did not return True.")
@@ -33,8 +36,11 @@ def exec_steps(*args):
 
 
 def git_step(cmd, args_string):
-    """Return a step function that returns true if the given git command and
-    arg string e.g. 'push', 'origin master' returns nonzero exit code."""
+    """Return a step function representing a git command.
+
+    The resulting function returns true if the given git command and
+    arg string (e.g. 'push', 'origin master') returns nonzero exit code.
+    """
     def step():
         _, rtncode, _ = git.call(cmd, args_string)
         return rtncode == 0
@@ -49,7 +55,7 @@ def deignore_build_step():
             with open(".gitignore", "r") as f:
                 content = f.readlines()
             with open(".gitignore", "w") as f:
-                f.writelines(content[:-1])
+                f.writelines(content[:-2])
         except Exception as e:
             logger.fatal(str(e))
             return False
@@ -65,6 +71,7 @@ def ignore_build_step():
             with open(".gitignore", "r") as f:
                 content = f.readlines()
             content.append("build/*")
+            content.append("blog/bower_components")
             with open(".gitignore", "w") as f:
                 f.writelines(content)
         except Exception as e:
@@ -75,6 +82,7 @@ def ignore_build_step():
 
 
 def subprocess_step(*args):
+    """Step for calling a subprocess."""
     def step():
         logger.info(" ".join(args))
         p = subprocess.Popen(args)
@@ -83,15 +91,18 @@ def subprocess_step(*args):
 
 
 def main():
+    """Main."""
     logger.info("Checking current branch...")
     current_branch = get_current_branch()
     if current_branch != "master":
-        logger.fatal("Must run this script from master. Currently on %s." % current_branch)
+        logger.fatal("Must run this script from master. Currently on %s." % current_branch) # noqa
         return
 
     logger.info("Checking for uncommitted work...")
     if has_uncommitted_changes():
-        logger.fatal("`git status -s` reports uncommitted work. Fix this to push.")
+        logger.fatal(
+            "`git status -s` reports uncommitted work. Fix this to push."
+        )
         return
 
     logger.info("Pushing...")
@@ -102,7 +113,7 @@ def main():
         subprocess_step("python", "scripts/build.py"),
         git_step("add", "build/*"),
         ignore_build_step(),
-        git_step("commit", "-m '[%s] Build for release'" % datetime.today().strftime("%c")),
+        git_step("commit", "-m '[%s] Build for release'" % datetime.today().strftime("%c")), # noqa
         git_step("push", "origin gh-pages"),
         git_step("checkout", "master"),
     )
