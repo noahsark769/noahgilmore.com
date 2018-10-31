4c378e4833e8d6c085c4f8b24526cfff9cfb090e
Noah Gilmore <noah.w.gilmore@gmail.com>
2018-10-30 21:39:40 -0700

    Shitty nav styles

--- blog-gatsby/src/components/Nav.jsx
+++ blog-gatsby/src/components/Nav.jsx
@@ -4,10 +4,15 @@ import styled from "styled-components";
 
 const StyledNav = styled.nav`
     display: flex;
-    flex-direction: horizontal;
+    flex-direction: row;
     flex-wrap: wrap;
     padding: 20px 60px;
     border-bottom: 1px solid #ddd;
+
+    @media all and (max-width: 600px) {
+        flex-direction: column;
+        padding: 20px 20px 10px 20px;
+    }
 `;
 
 const Title = styled.div`
@@ -22,6 +27,10 @@ const NavLinks = styled(UL)`
     flex-direction: horizontal;
     flex-wrap: wrap;
     justify-content: flex-end;
+
+    @media all and (max-width: 600px) {
+        justify-content: flex-start;
+    }
 `;
 
 const NavLink = styled(LI)`
@@ -31,6 +40,15 @@ const NavLink = styled(LI)`
     font-family: "Bariol", "Helvetica Neue", "Helvetica", sans-serif;
     font-size: 16px;
     padding: 0px 20px;
+
+    @media all and (max-width: 600px) {
+        display: block;
+        width: 100%;
+        font-size: 20px;
+        margin: 5px 0px;
+        padding: 10px 10px;
+        background-color: #eee;
+    }
 `;
 
 const StyledA = styled(A)`
@@ -39,6 +57,10 @@ const StyledA = styled(A)`
 
     &:hover {
         color: rgba(151, 195, 210, 1);
+    }
+
+    @media all and (max-width: 600px) {
+        width: 100%;
     }
 `;
 
