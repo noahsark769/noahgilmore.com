b0d60e7aef03572db112e2f0f95fa62ac7e9e0b7
Noah Gilmore <noah.w.gilmore@gmail.com>
2018-11-16 21:46:07 -0800

    CSS reset and a better 404 page

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,10 +1,45 @@
 import React from 'react'
+import styled from 'styled-components';
+
+const OuterContainer = styled.div`
+    margin: 0;
+    padding: 0;
+    width: 100%;
+    background-color: #3070a5;
+    height: 100vh;  
+`;
+
+
+const Container = styled.div`
+    margin: 0;
+    padding: 0;
+    width: 600px;
+    margin: 0 auto;
+    height: 100vh;
+    color: white;
+    font-family: Helvetica, sans-serif;
+    padding-top: 300px;
+`;
+
+const Title = styled.h1`
+    font-size: 100px;
+    padding-bottom: 40px;
+`;
+
+const A = styled.a`
+    &, &:hover, &:visited {
+        color: white;
+    }
+    text-decoration: underline;
+`;
 
 const NotFoundPage = () => (
-    <div>
-        <h1>:(</h1>
-        <p>You found a page that doesn't exist (404)</p>
-    </div>
+    <OuterContainer>
+        <Container>
+            <Title>:(</Title>
+            <p>You found a page that doesn't exist (404). <A href="/blog">Go back home</A></p>
+        </Container>
+    </OuterContainer>
 )
 
 export default NotFoundPage
