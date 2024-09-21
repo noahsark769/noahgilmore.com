ab87878092020be11ead981b002110d8bd84a9f2
Noah Gilmore <noah.w.gilmore@gmail.com>
2024-09-21 13:44:18 -0700

    Add in next on pages thing

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
+import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev'
+
 const nextConfig = {
   reactStrictMode: true,
+}
+
+if (process.env.NODE_ENV === 'development') {
+  await setupDevPlatform()
 }
 
 export default nextConfig
